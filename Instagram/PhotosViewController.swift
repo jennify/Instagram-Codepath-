ad2abb059459886f3d683ed09940cd8f65a44d05
Jennifer Lee <jlee@dropbox.com>
2016-02-03 23:32:49 -0800

    Bonus 1: User profiles and Images/ sections

--- Instagram/PhotosViewController.swift
+++ Instagram/PhotosViewController.swift
@@ -38,7 +38,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                     data, options:[]) as? NSDictionary {
-                        self.data = responseDictionary["data"] as! [NSDictionary]
+                        self.data = responseDictionary["data"] as? [NSDictionary]
                         self.tableView.reloadData()
                         
                     }
@@ -96,32 +96,87 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("com.codepath.PrototypeCell", forIndexPath: indexPath) as! TableViewCell
         if  let data = self.data {
-            let data_row = data[indexPath.row]
-            let caption_field = data_row["caption"] as? NSDictionary
-            let from_field = caption_field!["from"] as? NSDictionary
-            let url = from_field!["profile_picture"] as? String
-            // In memory of our janky code:
-            // let url = data["caption"]!["from"]!!["profile_picture"] as! String
+            let url = getURLFromData(indexPath.section)
             cell.photoImageView.setImageWithURL(NSURL(string: url!)!)
         }
         return cell
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func getURLFromData(index: Int) -> String? {
+        if  let data = self.data {
+            let data_row = data[index]
+            let caption_field = data_row["caption"] as? NSDictionary
+            let from_field = caption_field!["from"] as? NSDictionary
+            let url = from_field!["profile_picture"] as? String
+            return url
+        }
+        return nil
+
+    }
+    func getUsernameFromData(index: Int) -> String? {
+        if  let data = self.data {
+            let data_row = data[index]
+            print(data_row)
+            let caption_field = data_row["caption"] as? NSDictionary
+            let from_field = caption_field!["from"] as? NSDictionary
+            let username = from_field!["username"] as? String
+            return username
+        }
+        return nil
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         if let data = self.data {
             return data.count
         } else {
             return 0
         }
-
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        headerView.backgroundColor = UIColor(white: 1, alpha: 0.9)
         
+        let profileView = UIImageView(frame: CGRect(x: 10, y: 10, width: 30, height: 30))
+        profileView.clipsToBounds = true
+        profileView.layer.cornerRadius = 15;
+        profileView.layer.borderColor = UIColor(white: 0.7, alpha: 0.8).CGColor
+        profileView.layer.borderWidth = 1;
+        
+        // Use the section number to get the right URL
+        let url = self.getURLFromData(section)!
+        profileView.setImageWithURL(NSURL(string: url)!)
+        
+        headerView.addSubview(profileView)
+        
+        // Add a UILabel for the username here
+        let usernameLabel = UILabel(frame: CGRect(x: 50, y: 10, width: 280, height: 30))
+        usernameLabel.text = self.getUsernameFromData(section)
+        
+        headerView.addSubview(usernameLabel)
+        
+        
+        return headerView
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let vc = segue.destinationViewController as! PhotoDetailsViewController
         let indexPath = tableView.indexPathForCell(sender as! UITableViewCell)
         if  self.data != nil {
-            let data = self.data![indexPath!.row]
+            let data = self.data![indexPath!.section]
             let url = data["caption"]!["from"]!!["profile_picture"] as! String
             vc.photo_url = url
         }
